866c6bac899a43af0338a976aa370ebbae136fa5
hirokiinoue <hirokiinoue@outlook.it>
2016-12-21 15:06:05 +0100

    mod commit da corso.

--- javaShop/src/utenti/Admin.java
+++ javaShop/src/utenti/Admin.java
@@ -16,7 +16,7 @@ public class Admin extends Utente{
 
 	@Override //Categoria si intende il padre dell'articolo cercato
 	public ArrayList<Articolo> ricerca(String categoria, double prezzoMassimo, int quantitaMinima, boolean controllavisibilita, String nome) {
-// qusto è stato modificato............
+// qusto è stato modificato............ HIROKI ha mod ficato.
 		
 			ArrayList<Articolo> lista = this.dao.articoli(categoria);
 			ArrayList<Articolo> ris = new ArrayList<Articolo>();
